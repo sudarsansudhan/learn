08da54dcdfd9bad7368635b82ffb0c86edd7cd91
emb-sudasub <sudarshan.subramani@embitel.com>
2023-08-17 20:30:11 +0530

    add  the Employee class variable d

--- src/pa/Employee.java
+++ src/pa/Employee.java
@@ -5,6 +5,8 @@ public class Employee {
         int a=10;
         int b=39;
         int c=a+b;
+        int d=a*b;
         System.out.println("add a + b ="+c);
+        System.out.println("add a * b ="+d);
     }
 }
